ab9064503d0e5ac41b1d2c2d0e9f43f99d2ecee9
Sushi Chanrai <sushi@pathworks.co>
2018-06-25 14:16:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif !string.upcase && string !== "I LOVE YOU GRANDMA!"
+  elsif !string.upcase && string != "I LOVE YOU GRANDMA!"
     return "HUH?! SPEAK UP, SONNY!"
    elsif string.upcase 
     return "NO, NOT SINCE 1938!"
