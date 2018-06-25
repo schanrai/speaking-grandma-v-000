5fa5bead04acbad2720b572ae98934c5195aad52
Sushi Chanrai <sushi@pathworks.co>
2018-06-25 14:12:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
    elsif string.upcase 
-    puts"NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
    elsif !string.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   end 
